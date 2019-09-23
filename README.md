4d91682ff8d75529e5f1b9f4d9e003c17350e9d9
Archis Gore <archis@polyverse.io>
2019-09-23 08:55:12 -0700

    Added examples in readme.

--- README.md
+++ README.md
@@ -1,2 +1,167 @@
-# timed-iterator
-A Rust Timed Iterator that can timeout on next(). Useful for reading bursty streams (Device files, network streams, etc.)
+# timeout-iterator
+
+`timeout_iterator::TimeoutIterator` is a wrapper over any iterator that adds two additional functions:
+* peek_timeout()
+* next_timeout()
+
+The canonical use-case is parsing multi-line free-form records (such as tailing a log fime) where it is desirable to 
+consume the very last line, and peek whether the record continues on the next time, without blocking indefinitely on the peek().
+
+This was built for parsing Kernel logs from `/dev/kmsg` for instance. A kernel log record may look like this:
+
+```text
+6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+ SUBSYSTEM=pci
+ DEVICE=+pci:0000:00:05.0
+ ```
+The end of such a record is only known when the next line begins a new record. However, if this were the last
+record emitted, then it is possible that it never gets flushed/parsed because there is no next record to trigger it.
+
+This is where an iterator with a timeout capability helps to break the deadlock.
+
+# Basic Iteration
+
+There are two primary ways of constructing and using the iterator.
+
+## The Item Iterator
+
+The item iterator is obvious and intuitive:
+
+```
+use timeout_iterator::TimeoutIterator;
+
+let numbers: Vec<u32> = vec![1, 2, 3, 4, 5];
+let mut ti = TimeoutIterator::from_item_iterator(numbers.into_iter(), 0);
+```
+
+This will iterate over all the integers in the Vector.
+
+## The Result Iterator
+
+Occasionally, your downstream iterator returns a Result<Item, _>, such as when 
+we read lines from a BufRead and we have to unwrap twice:
+
+```
+use timeout_iterator::TimeoutIterator;
+use std::io::prelude::BufRead;
+
+let logmessage = 
+r"6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+SUBSYSTEM=pci
+DEVICE=+pci:0000:00:05.0";
+
+let mut lines_iterator = (Box::new(logmessage.as_bytes()) as Box<dyn BufRead + Send>).lines();
+let result = lines_iterator.next()    
+    .unwrap() // open the timeout iterator option
+    .ok() // open the line-iterator result
+    .unwrap(); // open the line-iterator option
+
+assert_eq!(result, "6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode");
+```
+
+This can get worse when using *_timeout() functions, because they return a Result<> themselves, which
+then further wraps another result. An item iterator from a timeout would unpack something like this:
+
+```
+use timeout_iterator::TimeoutIterator;
+use std::io::prelude::BufRead;
+use std::time::Duration;
+
+let logmessage = 
+r"6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+SUBSYSTEM=pci
+DEVICE=+pci:0000:00:05.0";
+
+let mut lines_iterator = (Box::new(logmessage.as_bytes()) as Box<dyn BufRead + Send>).lines();
+let mut ti = TimeoutIterator::from_item_iterator(lines_iterator, 0);
+let result = ti.next_timeout(Duration::from_secs(1))
+    .ok() // open the timeout iterator result
+    .unwrap() // open the timeout iterator option
+    .ok() // open the line-iterator result
+    .unwrap(); // open the line-iterator option
+
+assert_eq!(result, "6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode");
+```
+
+Knowing that the inner iterator provides `Result<Option<Foo>>`, we can tell the TimeoutIterator to 
+flatten it all into this:
+```
+use timeout_iterator::TimeoutIterator;
+use std::io::prelude::BufRead;
+
+let logmessage = 
+r"6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+SUBSYSTEM=pci
+DEVICE=+pci:0000:00:05.0";
+
+let mut lines_iterator = (Box::new(logmessage.as_bytes()) as Box<dyn BufRead + Send>).lines();
+let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
+
+let result = ti.next()
+    .unwrap(); // single unwrap of Option
+
+assert_eq!(result, "6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode");
+```
+
+
+# Effectively using Timeouts
+
+Now let's look at two log "Records", split over five lines:
+
+```ignore
+6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+ SUBSYSTEM=pci
+ DEVICE=+pci:0000:00:05.0
+6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+ DEVICE=+pci:0000:00:05.0
+```
+
+ We have an iterator over these lines, and we want to flush the second record cleanly, if no new
+ lines are found within the next second (at that point, any future lines are for the next record).
+
+ ```
+use timeout_iterator::TimeoutIterator;
+use std::io::prelude::BufRead;
+use std::time::Duration;
+
+let logmessage = 
+r"6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+  SUBSYSTEM=pci
+  DEVICE=+pci:0000:00:05.0
+6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode
+  DEVICE=+pci:0000:00:05.0";
+
+let mut lines_iterator = (Box::new(logmessage.as_bytes()) as Box<dyn BufRead + Send>).lines();
+let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
+
+let mut records: Vec<String> = vec![];
+
+loop {
+    match ti.next_timeout(Duration::from_secs(1)) {
+        Ok(next_line) => {
+            let mut record = String::from(next_line);
+            loop {
+                match ti.peek_timeout(Duration::from_secs(1)) {
+                    Ok(maybe_continuation_line) => {
+                        if maybe_continuation_line.starts_with(' ') {
+                            record.push('\n');
+                            record.push_str(maybe_continuation_line);
+                            ti.next(); //flush it
+                        } else {
+                            break;
+                        }
+                    },
+                    Err(_) => break
+                }
+            }
+            records.push(record);
+        },
+        Err(_) => break
+    }
+}
+
+assert_eq!(records.remove(0), "6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode\n SUBSYSTEM=pci\n DEVICE=+pci:0000:00:05.0");
+
+assert_eq!(records.remove(0), "6,361,518496,-;ahci 0000:00:05.0: AHCI 0001.0300 32 slots 6 ports 6 Gbps 0x1 impl SATA mode\n DEVICE=+pci:0000:00:05.0");
+
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,194 +1,215 @@
-pub mod timeout_iterator {
-    use std::sync::mpsc::{Sender, Receiver};
-    use std::sync::mpsc;
-    use std::thread;
-    use std::time::{Duration};
-    use std::result;
-    use std::fmt;
+#[macro_use]
+extern crate doc_comment;
 
-    pub enum TimeoutIteratorError {
-        Timeout,
-        Disconnected,
-    }
+doc_comment!(include_str!("../README.md"));
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration};
+use std::result;
+use std::fmt;
 
-    pub struct TimeoutIterator<T> {
-        source: Receiver<T>,
-        buffer: Vec<T>,
-        verbosity: u8,
-    }
+pub enum TimeoutIteratorError {
+    Timeout,
+    Disconnected,
+}
 
-    impl<T> TimeoutIterator<T> 
-    where T: Send + 'static {
-        pub fn from_result_iterator<R, E>(reader: R, verbosity: u8) -> TimeoutIterator<T>
-        where R: Iterator<Item = result::Result<T, E>> + Send + 'static,
-            E: fmt::Display + fmt::Debug {
-            let (sink, source) : (Sender<T>, Receiver<T>) = mpsc::channel();
+pub struct TimeoutIterator<T> {
+    source: Receiver<T>,
+    buffer: Vec<T>,
+    verbosity: u8,
+}
 
-            thread::spawn(move || {
-                for maybe_item in reader {
-                    match maybe_item {
-                        Ok(item) => if let Err(e) = sink.send(item) {
-                            if verbosity > 0 { eprintln!("TimeoutIterator:: Error sending data to channel. Receiver may have closed. Closing up sender. Error: {}", e); }
-                            return;
-                        },
-                        Err(e) => {
-                            if verbosity > 0 { eprintln!("TimeoutIterator:: Error reading from source iterator. Closing reader. Error: {}", e); }
-                            return;
-                        }
-                    }
-                }
-            });
+impl<T> TimeoutIterator<T> 
+where T: Send + 'static {
 
-            TimeoutIterator {
-                source,
-                verbosity,
-                buffer: Vec::new()
-            }
-        }
+    /**
+     * Use this constructor 
+     */
+    pub fn from_result_iterator<R, E>(reader: R, verbosity: u8) -> TimeoutIterator<T>
+    where R: Iterator<Item = result::Result<T, E>> + Send + 'static,
+        E: fmt::Display + fmt::Debug {
+        let (sink, source) : (Sender<T>, Receiver<T>) = mpsc::channel();
 
-        pub fn from_item_iterator<R>(reader: R, verbosity: u8) -> TimeoutIterator<T>
-        where R: Iterator<Item = T> + Send + 'static {
-            let (sink, source) : (Sender<T>, Receiver<T>) = mpsc::channel();
-
-            thread::spawn(move || {
-                for item in reader {
-                    if let Err(e) = sink.send(item) {
+        thread::spawn(move || {
+            for maybe_item in reader {
+                match maybe_item {
+                    Ok(item) => if let Err(e) = sink.send(item) {
                         if verbosity > 0 { eprintln!("TimeoutIterator:: Error sending data to channel. Receiver may have closed. Closing up sender. Error: {}", e); }
+                        return;
+                    },
+                    Err(e) => {
+                        if verbosity > 0 { eprintln!("TimeoutIterator:: Error reading from source iterator. Closing reader. Error: {}", e); }
                         return;
                     }
                 }
-            });
-
-            TimeoutIterator {
-                source,
-                verbosity,
-                buffer: Vec::new()
             }
+        });
+
+        TimeoutIterator {
+            source,
+            verbosity,
+            buffer: Vec::new()
         }
+    }
 
-        pub fn next_timeout(&mut self, timeout: Duration) -> Result<T, TimeoutIteratorError> {
-            if !self.buffer.is_empty() {
-                return Ok(self.buffer.remove(0));
-            };
+    pub fn from_item_iterator<R>(reader: R, verbosity: u8) -> TimeoutIterator<T>
+    where R: Iterator<Item = T> + Send + 'static {
+        let (sink, source) : (Sender<T>, Receiver<T>) = mpsc::channel();
 
+        thread::spawn(move || {
+            for item in reader {
+                if let Err(e) = sink.send(item) {
+                    if verbosity > 0 { eprintln!("TimeoutIterator:: Error sending data to channel. Receiver may have closed. Closing up sender. Error: {}", e); }
+                    return;
+                }
+            }
+        });
+
+        TimeoutIterator {
+            source,
+            verbosity,
+            buffer: Vec::new()
+        }
+    }
+
+    pub fn next_timeout(&mut self, timeout: Duration) -> Result<T, TimeoutIteratorError> {
+        if !self.buffer.is_empty() {
+            return Ok(self.buffer.remove(0));
+        };
+
+        match self.source.recv_timeout(timeout) {
+            Ok(item) => Ok(item),
+            Err(e) => match e {
+                mpsc::RecvTimeoutError::Timeout => Err(TimeoutIteratorError::Timeout),
+                mpsc::RecvTimeoutError::Disconnected => Err(TimeoutIteratorError::Disconnected)
+            }
+        }            
+    }
+
+    pub fn peek_timeout(&mut self, timeout: Duration) -> Result<&T, TimeoutIteratorError> {
+        if self.buffer.is_empty() {
             match self.source.recv_timeout(timeout) {
-                Ok(item) => Ok(item),
+                Ok(item) => self.buffer.push(item),
                 Err(e) => match e {
-                    mpsc::RecvTimeoutError::Timeout => Err(TimeoutIteratorError::Timeout),
-                    mpsc::RecvTimeoutError::Disconnected => Err(TimeoutIteratorError::Disconnected)
+                    mpsc::RecvTimeoutError::Timeout => return Err(TimeoutIteratorError::Timeout),
+                    mpsc::RecvTimeoutError::Disconnected => return Err(TimeoutIteratorError::Disconnected)
                 }
-            }            
-        }
+            }   
+        };
 
-        pub fn peek_timeout(&mut self, timeout: Duration) -> Result<&T, TimeoutIteratorError> {
-            if self.buffer.is_empty() {
-                match self.source.recv_timeout(timeout) {
-                    Ok(item) => self.buffer.push(item),
-                    Err(e) => match e {
-                        mpsc::RecvTimeoutError::Timeout => return Err(TimeoutIteratorError::Timeout),
-                        mpsc::RecvTimeoutError::Disconnected => return Err(TimeoutIteratorError::Disconnected)
-                    }
-                }   
-            };
-
-            Ok(self.buffer.first().unwrap())
-        }
+        Ok(self.buffer.first().unwrap())
     }
+}
 
 
-    impl<T> Iterator for TimeoutIterator<T> {
-        type Item = T;
-        fn next(&mut self) -> Option<Self::Item> {
-            if !self.buffer.is_empty() {
-                return Some(self.buffer.remove(0));
-            };
+impl<T> Iterator for TimeoutIterator<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.buffer.is_empty() {
+            return Some(self.buffer.remove(0));
+        };
 
-            match self.source.recv() {
-                Ok(item) => {
-                    Some(item)
-                },
-                Err(e) => {
-                    if self.verbosity > 0 {
-                        eprintln!("TimeoutIterator:: Error occurred reading from source: {}.", e);
-                    }
-                    None
+        match self.source.recv() {
+            Ok(item) => {
+                Some(item)
+            },
+            Err(e) => {
+                if self.verbosity > 0 {
+                    eprintln!("TimeoutIterator:: Error occurred reading from source: {}.", e);
                 }
+                None
             }
         }
     }
+}
 
-    #[cfg(test)]
-    mod tests {
-        use std::io::prelude::*;
-        use super::*;
+#[cfg(test)]
+mod tests {
+    use std::io::prelude::*;
+    use super::*;
 
-        #[test]
-        fn iterates() {
-            let realistic_message = 
+    #[test]
+    fn iterates() {
+        let realistic_message = 
 r"1
 2
 3
 4
 5";
-            let lines_iterator = (Box::new(realistic_message.as_bytes()) as Box<dyn BufRead + Send>).lines();
+        let lines_iterator = (Box::new(realistic_message.as_bytes()) as Box<dyn BufRead + Send>).lines();
 
-            let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
+        let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
 
-            assert_eq!(ti.next().unwrap(), "1");
-            assert_eq!(ti.next().unwrap(), "2");
-            assert_eq!(ti.next().unwrap(), "3");
-            assert_eq!(ti.next().unwrap(), "4");
-            assert_eq!(ti.next().unwrap(), "5");
-        }
-
-        #[test]
-        fn next_timeout() {
-            let realistic_message = 
-r"1
-2
-3
-4
-5";
-            let lines_iterator = (Box::new(realistic_message.as_bytes()) as Box<dyn BufRead + Send>).lines();
-
-            let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
-
-            assert_eq!(ti.next().unwrap(), "1");
-            assert_eq!(ti.next().unwrap(), "2");
-            assert_eq!(ti.next().unwrap(), "3");
-            assert_eq!(ti.next().unwrap(), "4");
-            assert_eq!(ti.next().unwrap(), "5");
-
-            let timeout_result = ti.next_timeout(Duration::from_secs(1));
-            assert!(timeout_result.is_err());
-        }
-
-            #[test]
-        fn peek_doesnt_remove() {
-            let realistic_message = 
-r"1
-2
-3
-4
-5";
-            let lines_iterator = (Box::new(realistic_message.as_bytes()) as Box<dyn BufRead + Send>).lines();
-
-            let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
-
-            assert_eq!(ti.next().unwrap(), "1");
-            assert_eq!(ti.next().unwrap(), "2");
-            assert_eq!(ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), "3");
-            assert_eq!(ti.next().unwrap(), "3");
-            assert_eq!(ti.next().unwrap(), "4");
-            assert_eq!(ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), "5");
-            assert_eq!(ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), "5");
-            assert_eq!(ti.next().unwrap(), "5");
-
-            let timeout_result = ti.next_timeout(Duration::from_secs(1));
-            assert!(timeout_result.is_err());
-        }
+        assert_eq!(ti.next().unwrap(), "1");
+        assert_eq!(ti.next().unwrap(), "2");
+        assert_eq!(ti.next().unwrap(), "3");
+        assert_eq!(ti.next().unwrap(), "4");
+        assert_eq!(ti.next().unwrap(), "5");
     }
 
+    #[test]
+    fn next_timeout() {
+        let realistic_message = 
+r"1
+2
+3
+4
+5";
+        let lines_iterator = (Box::new(realistic_message.as_bytes()) as Box<dyn BufRead + Send>).lines();
 
+        let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
 
+        assert_eq!(ti.next().unwrap(), "1");
+        assert_eq!(ti.next().unwrap(), "2");
+        assert_eq!(ti.next().unwrap(), "3");
+        assert_eq!(ti.next().unwrap(), "4");
+        assert_eq!(ti.next().unwrap(), "5");
+
+        let timeout_result = ti.next_timeout(Duration::from_secs(1));
+        assert!(timeout_result.is_err());
+    }
+
+        #[test]
+    fn peek_doesnt_remove() {
+        let realistic_message = 
+r"1
+2
+3
+4
+5";
+        let lines_iterator = (Box::new(realistic_message.as_bytes()) as Box<dyn BufRead + Send>).lines();
+        let mut ti = TimeoutIterator::from_result_iterator(lines_iterator, 0);
+
+        assert_eq!(ti.next().unwrap(), "1");
+        assert_eq!(ti.next().unwrap(), "2");
+        assert_eq!(ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), "3");
+        assert_eq!(ti.next().unwrap(), "3");
+        assert_eq!(ti.next().unwrap(), "4");
+        assert_eq!(ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), "5");
+        assert_eq!(ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), "5");
+        assert_eq!(ti.next().unwrap(), "5");
+
+        let timeout_result = ti.next_timeout(Duration::from_secs(1));
+        assert!(timeout_result.is_err());
+    }
 }
+
+#[test]
+fn item_iterator() {
+    let numbers: Vec<u32> = vec![1, 2, 3, 4, 5];
+    let mut ti = TimeoutIterator::from_item_iterator(numbers.into_iter(), 0);
+
+    assert_eq!(ti.next().unwrap(), 1);
+    assert_eq!(ti.next().unwrap(), 2);
+    assert_eq!(*ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), 3);
+    assert_eq!(ti.next().unwrap(), 3);
+    assert_eq!(ti.next().unwrap(), 4);
+    assert_eq!(*ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), 5);
+    assert_eq!(*ti.peek_timeout(Duration::from_secs(1)).ok().unwrap(), 5);
+    assert_eq!(ti.next().unwrap(), 5);
+
+    let timeout_result = ti.next_timeout(Duration::from_secs(1));
+    assert!(timeout_result.is_err());
+}
+
